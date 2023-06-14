2c7509bebcc5a8075c36a5ee04a3d0f4ca18535e
DavideAtzo <atzo9477@gmail.com>
2023-06-14 17:16:19 +0200

    creazione migration product

---
+++ database/migrations/2023_06_14_145845_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('name', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', 4, 2);
+            $table->boolean('gluten_free');
+            $table->boolean('vegan');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
