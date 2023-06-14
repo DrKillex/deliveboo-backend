dd05c2ef2862473dc1546e4629e5468957203739
alessandroiero <alessandro.iero@outlook.it>
2023-06-14 18:16:49 +0200

    Order Seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class OrderSeeder extends Seeder
 {
@@ -12,8 +14,21 @@ class OrderSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<15; $i++){
+            $newOrder= new Order();
+
+            $newOrder->name=$faker->name();
+            $newOrder->surname=$faker->lastName();
+            $newOrder->address=$faker->address();
+            $newOrder->email=$faker->email();
+            $newOrder->telephone=$faker->phoneNumber();
+            $newOrder->total_price=$faker->randomFloat(2, 5, 100);
+            $newOrder->payment_state=$faker->numberBetween(0, 1);
+
+            $newOrder->save();
+        }
+
     }
 }
