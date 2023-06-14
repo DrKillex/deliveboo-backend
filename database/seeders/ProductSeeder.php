d24982e39a610ec61ea474a23117076082bccfc2
DavideAtzo <atzo9477@gmail.com>
2023-06-14 17:44:29 +0200

    seeder faker

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
@@ -12,8 +15,18 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++){
+
+            $product = new Product();
+            $product->name = $faker->word;
+            $product->description = $faker->text(100);
+            $product->price = $faker->randomFloat(2, 5, 100);
+            $product->gluten_free = $faker->numberBetween(0, 1);
+            $product->vegan = $faker->numberBetween(0, 1);
+            $product->slug = Str::slug($product->name, '-');
+            $product->save();
+        }
     }
 }
