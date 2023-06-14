833339bf211f9d1c84bf73a25ef73c2ae00ab78b
alessandroiero <alessandro.iero@outlook.it>
2023-06-14 18:36:09 +0200

    Requests Orders

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'surname' => 'required|string|max:50',
+            'address' => 'required|max:150',
+            'email' => 'nullable|max:100',
+            'telephone' => 'required|max:15',
+            'total_price' => 'required|numeric',
+            'payment_state' => 'required|boolean'
+        ];
+    }
+}
