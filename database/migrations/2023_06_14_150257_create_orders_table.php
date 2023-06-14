69c3631b274d9c6588811ef72a786808c801eda2
alessandroiero <alessandro.iero@outlook.it>
2023-06-14 17:29:14 +0200

    Migration Orders

--- database/migrations/2023_06_14_150257_create_orders_table.php
+++ database/migrations/2023_06_14_150257_create_orders_table.php
@@ -16,7 +16,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            
+
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('address', 150);
+            $table->string('email', 100);
+            $table->string('telephone',15);
+            $table->decimal('total_price', 6,2);
+            $table->boolean('payment_state')->default(false);
+
             $table->timestamps();
         });
     }
