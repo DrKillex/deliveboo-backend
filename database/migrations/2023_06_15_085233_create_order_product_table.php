d4324e5cd1d1d8bf659ce4705f8de9b6866cddb9
alessandroiero <alessandro.iero@outlook.it>
2023-06-15 11:18:43 +0200

    Creata Tabella Many To Many Order-Product

---
+++ database/migrations/2023_06_15_085233_create_order_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            // $table->id();
+
+            // se viene eliminato un order vengono eliminate anche tutte le righe dove presente order_id
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            
+            // senza una chiave primaria la creiamo noi con l'array formato da order id e product id
+            $table->primary(['order_id', 'product_id']);
+            
+            $table->dateTime('date');
+            $table->unsignedTinyInteger('quantity');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
