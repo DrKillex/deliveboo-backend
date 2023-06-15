ca0682ff88ec5b25666b29edd0399a342cac4b2c
DavideAtzo <atzo9477@gmail.com>
2023-06-15 10:49:25 +0200

    creazione tabella pivot category-restaurant

---
+++ database/migrations/2023_06_15_084643_create_category_restaurant_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_restaurant', function (Blueprint $table) {
+            $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->primary(['restaurant_id', 'category_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_restaurant');
+    }
+};
