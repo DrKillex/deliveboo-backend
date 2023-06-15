30fbdbb015b5d912dc5235ec390f85a6a36e68cf
RosaD97 <r960595@gmail.com>
2023-06-15 15:49:29 +0200

    feat-api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('restaurants', [RestaurantController::class, 'index']);
+Route::get('restaurant/{slug}', [RestaurantController::class, 'show']);
+
+
