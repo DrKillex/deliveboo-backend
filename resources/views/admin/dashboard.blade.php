03444f9c86b44578025d6cf52c375434f85e7927
DrKillex <lcollaro1994@gmail.com>
2023-06-21 12:58:01 +0200

    reinderizzamento

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid mt-4">
+@if (!$restaurant)
+{{-- <div class="container-fluid mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -16,18 +17,29 @@
                     {{ __('You are logged in!') }}
 
                         
-                        @if ($restaurant)
+                        
                             @foreach ($restaurant as $res)
                                 <div><a href="{{ route('admin.restaurants.show', $res) }}">{{$res->name}}</a></div>
                             @endforeach
                         @else
                             <div><a class="nav-link" href="{{ route('admin.restaurants.create') }}">{{ __('Nuovo Ristorante') }}</a></div>
-                        @endif
+                        
 
                 
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+@php
+header("Location: " . URL::to('/admin/restaurants/create'), true, 302);
+exit();
+@endphp
+@else
+@php
+header("Location: " . URL::to('/admin/restaurants/'.$restaurant[0]->slug), true, 302);
+exit();
+@endphp
+
+@endif
 @endsection
