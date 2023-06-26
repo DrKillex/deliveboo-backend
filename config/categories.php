9dc7f742c0ca95d256709cec8c73020671925fe6
DavideAtzo <atzo9477@gmail.com>
2023-06-26 17:40:15 +0200

    seeder category in modo brutto

---
+++ config/categories.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    [
+        "name" => "Italiano",
+        "description" => "La cucina italiana è una delle più rinomate al mondo, apprezzata per la sua varietà, freschezza e sapori autentici. La tradizione culinaria italiana si basa su ingredienti di alta qualità e sulla semplicità, che permette agli ingredienti di brillare e di esaltare i sapori naturali.",
+    ],
+    [
+        "name" => "Internazionale",
+        "description" => "La cucina internazionale comprende una vasta gamma di stili culinari provenienti da diverse culture e paesi di tutto il mondo. La sua caratteristica principale è la diversità, che si riflette nei sapori, negli ingredienti e nelle tecniche di preparazione utilizzate.",
+    ],
+    [
+        "name" => "Cinese",
+        "description" => "La cucina cinese è caratterizzata dalla varietà di sapori, ingredienti freschi e tecniche di preparazione tradizionali. Una delle caratteristiche distintive della cucina cinese è l'equilibrio tra i cinque sapori fondamentali: dolce, salato, acido, amaro e piccante. Questa armonia di sapori si riflette in molti piatti cinesi.",
+    ],
+    [
+        "name" => "Giapponese",
+        "description" => "La cucina giapponese è rinomata per la sua delicatezza, l'attenzione ai dettagli e la freschezza degli ingredienti. È una cucina che combina sapientemente gusto, presentazione e equilibrio nutrizionale. Uno dei pilastri della cucina giapponese è il sushi, che ha guadagnato popolarità in tutto il mondo. Il sushi è fatto di riso condito con aceto di riso, accompagnato da pesce crudo o altri ingredienti come verdure e uova. È spesso servito con salsa di soia, wasabi e zenzero marinato.",
+    ],
+    [
+        "name" => "Messicano",
+        "description" => "È una cucina che unisce influenze indigene, spagnole e di altre culture mescolando ingredienti locali e tecniche di cottura tradizionali. Uno dei piatti più emblematici della cucina messicana è il taco, che consiste in una tortilla di mais ripiena di carne, pollo, pesce o verdure, accompagnata da salse, guacamole e altri condimenti. I tacos sono spesso piccanti e ricchi di sapori intensi.",
+    ],
+    [
+        "name" => "Indiano",
+        "description" => "La cucina indiana è conosciuta per la sua ricchezza di sapori, spezie e piatti aromatici. È una cucina variegata che si differenzia da regione a regione, offrendo una vasta gamma di specialità culinarie. Uno degli elementi distintivi della cucina indiana è l'uso di una varietà di spezie che conferiscono ai piatti un gusto unico.",
+    ],
+    [
+        "name" => "Pesce",
+        "description" => "Questa cucina si concentra sull'utilizzo di una vasta gamma di pesci e frutti di mare, offrendo una varietà di sapori, testure e preparazioni.",
+    ],
+    [
+        "name" => "Carne",
+        "description" => "Questa cucina si concentra sull'utilizzo di carne proveniente da diverse fonti, come manzo, maiale, pollo, agnello e altre carni più esotiche.",
+    ],
+    [
+        "name" => "Pizza",
+        "description" => " La pizza è ampiamente diffusa in tutto il mondo e la pizza stessa è diventata uno dei piatti più popolari e riconoscibili globalmente",
+    ],
+    [
+        "name" => "Hawaiano",
+        "description" => "La cucina hawaiana è un mix unico di influenze culinarie provenienti da diverse culture, inclusi gli indigeni polinesiani, i popoli migranti e le influenze asiatiche. Questa cucina è caratterizzata da ingredienti freschi e locali, spesso legati al mare e alla ricca biodiversità dell'arcipelago hawaiano.",
+    ],
+    [
+        "name" => "Brasiliano",
+        "description" => "La cucina brasiliana è ricca di sapori, colori e influenze culturali. Essa si caratterizza per la sua diversità regionale e per l'uso di ingredienti freschi e abbondanti, grazie alla ricchezza del territorio brasiliano.",
+    ],
+    [
+        "name" => "Americano",
+        "description" => "Uno degli aspetti distintivi della cucina americana è l'enfasi sui cibi abbondanti e sostanziosi. I pasti tipici americani spesso includono una proteina principale, come carne di manzo, pollo, maiale o pesce, accompagnata da contorni come patate, riso, verdure e insalate. Una delle preparazioni più famose della cucina americana è il barbecue",
+    ],
+    [
+        "name" => "Fast Food",
+        "description" => "La cucina fast food si riferisce a uno stile di cucina che si caratterizza per la preparazione e il servizio di cibi rapidi e convenienti. È un tipo di cucina pensata per soddisfare la fame in modo veloce e spesso è associata a catene di ristoranti che offrono un menu standardizzato e ampiamente riconoscibile.",
+    ],
+    [
+        "name" => "Pasta",
+        "description" => "La pasta è un alimento versatile, realizzato con farina di grano e acqua (e talvolta uova), che viene lavorato e modellato in vari formati e tagli, come spaghetti, penne, fusilli, linguine, ravioli e molti altri.",
+    ],
+];
